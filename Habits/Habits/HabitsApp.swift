5503c437a76a039944827a2f950d629130f3cf26
Nicholas Chan <99701277+nicknahc@users.noreply.github.com>
2023-11-22 16:22:08 -0800

    project init

---
+++ Habits/Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Nicholas Chan on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
